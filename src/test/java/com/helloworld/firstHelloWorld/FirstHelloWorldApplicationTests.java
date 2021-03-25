633732be18bb1179a483e8f52f203e3b0eb3c05b
Laaapez <lopezram.96@gmail.com>
2021-03-24 21:36:46 -0300

    first commit hello world

---
+++ src/test/java/com/helloworld/firstHelloWorld/FirstHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.firstHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
